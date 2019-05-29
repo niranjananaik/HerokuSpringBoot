c7abe6ccfc245dbedbd151813ec378367bac26cb
niranjana <niranjana_naik@persistent.com>
2019-05-29 11:57:38 +0530

    request mapping=/

--- src/main/java/com/test/testProject/controller/AppController.java
+++ src/main/java/com/test/testProject/controller/AppController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String test() {
 		return "Hello World!";
 	}
